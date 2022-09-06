5ef4094b1ee666b840dbdd8b039c9062b8bd54a1
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-09-06 11:54:12 -0500

    Se crea entidad para mapear las entradas de un formulario.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/main/java/com/fmontalvoo/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/fmontalvoo/springboot/form/app/controllers/FormController.java
@@ -1,11 +1,11 @@
 package com.fmontalvoo.springboot.form.app.controllers;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.fmontalvoo.springboot.form.app.models.UserForm;
 
 @Controller
 public class FormController {
@@ -16,14 +16,9 @@ public class FormController {
 	}
 
 	@PostMapping("/form")
-	public String submit(HttpServletRequest request, Model model) {
-		String username = request.getParameter("username");
-		String email = request.getParameter("email");
-		String password = request.getParameter("password");
-		System.out.println(username + ',' + email + ',' + password);
-		model.addAttribute("username", username);
-		model.addAttribute("email", email);
-		model.addAttribute("password", password);
+	public String submit(UserForm userForm, Model model) {
+		
+		model.addAttribute("user", userForm);
 		return "submit";
 	}
 
